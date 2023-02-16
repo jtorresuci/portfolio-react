781161eb0f788467b0eccadfd87c8532acf02b29
Jose Torres <jtorresocc@gmail.com>
2023-02-16 01:39:50 -0800

    Columns look good but githubcards not showing up.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import { Container, Navbar } from 'react-bootstrap';
-
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import CustomNavbar from './components/CustomNavbar';
-// import NameTrail from './components/Name';
-import Logo from './components/Logo';
 import GithubCard from './components/GithubCard';
-// import Word from './components/ sWord';
+// import banner from '/banner.png';
 
 function App() {
   return (
-    <div  style={{
-      backgroundColor: '#111111', 
-    }}> 
-    <CustomNavbar/>
-    <Logo imageUrl="/banner.png" altText="My logo"/>
-    <GithubCard username="jtorresuci" />
+    <div style={{ backgroundColor: '#111111' }}>
+      <CustomNavbar />
+
+      <Container fluid>
+        <Row>
+          <Col md={3} className="bg-primary text-white d-flex flex-column p-4" style={{ height: '100vh' }}>
+            <div className="mb-4">
+              {/* <img src="/Banner.png" alt="My logo" width="80%" /> */}
+            </div>
+            <div className="mb-auto">
+              <h5 className="mb-3">Jose Torres</h5>
+              <p className="mb-3">
+                <i className="fa fa-phone"></i> (555) 555-5555
+              </p>
+              <p className="mb-3">
+                <i className="fa fa-envelope"></i> jose.torres@email.com
+              </p>
+              <p>
+                <i className="fa fa-map-marker"></i> 123 Main St, Anytown, USA
+              </p>
+            </div>
+          </Col>
+
+          <Col md={3} className="p-4">
+          <GithubCard username="jtorresuci" />
+
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
