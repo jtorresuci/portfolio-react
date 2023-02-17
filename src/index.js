92931bbae3fb44abec046ac2b6053da20d585d40
Jose Torres <jtorresocc@gmail.com>
2023-02-17 00:39:32 -0800

    Squashed bug in index.js 🐛

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './custom.css'; // add this line
 
 import './bootstrap.min.css'
 
