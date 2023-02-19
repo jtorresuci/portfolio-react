3004cd849f0e0ec0e32a6dbc0bc869a2f1aa820c
Jose Torres <jtorresocc@gmail.com>
2023-02-18 23:25:09 -0800

    Updated to follow on mobile.

--- src/components/AuroraTrail.js
+++ src/components/AuroraTrail.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTrail, animated } from 'react-spring';
 import Particles from './Particles';
 
 const AuroraTrail = () => {
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setIsDesktop(window.innerWidth > 768);
+  //   };
+
+  //   handleResize();
+
+  //   window.addEventListener('resize', handleResize);
+
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
   const onMouseMove = e => {
     setMousePos({ x: e.clientX, y: e.clientY });
   };
 
+  const onTouchMove = e => {
+    if (e.touches.length > 0) {
+      setMousePos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+    }
+  };
+
   return (
-    <div style={{ width: '100%', height: '100%' }} onMouseMove={onMouseMove}>
-      {mousePos && <Particles mousePos={mousePos} />}
+    <div
+      style={{ width: '100%', height: '100%' }}
+      onMouseMove={onMouseMove}
+      onTouchMove={onTouchMove}
+    >
+      {isDesktop && mousePos && <Particles mousePos={mousePos} />}
     </div>
   );
 };
