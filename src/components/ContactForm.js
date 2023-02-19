8b0c794a74d84af9a28dd789a5a0e36934726c29
Jose Torres <jtorresocc@gmail.com>
2023-02-18 22:59:00 -0800

    Contact form mvp ready.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,10 +5,35 @@ const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // You can add your logic to handle form submission here
+    try {
+      const response = await fetch('http://127.0.0.1:5000/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, message })
+      });
+      if (response.ok) {
+        setIsSubmitted(true);
+      } else {
+        setIsError(true);
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  const handleReset = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+    setIsSubmitted(false);
+    setIsError(false);
   };
 
   return (
@@ -16,23 +41,41 @@ const ContactForm = () => {
       <Row className="justify-content-center">
         <Col md={8}>
           <h2>Contact Us</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} required />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </Form.Group>
-            <Form.Group controlId="formMessage">
-              <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={4} placeholder="Enter your message" value={message} onChange={(e) => setMessage(e.target.value)} required />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+          {isSubmitted && (
+            <div className="alert alert-success" role="alert">
+              Thank you for your message! We will get back to you as soon as possible.
+              <button type="button" className="close" aria-label="Close" onClick={handleReset}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          )}
+          {isError && (
+            <div className="alert alert-danger" role="alert">
+              An error occurred while submitting the form. Please try again later.
+              <button type="button" className="close" aria-label="Close" onClick={handleReset}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          )}
+          {!isSubmitted && !isError && (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} required />
+              </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+              </Form.Group>
+              <Form.Group controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={4} placeholder="Enter your message" value={message} onChange={(e) => setMessage(e.target.value)} required />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          )}
         </Col>
       </Row>
     </Container>
